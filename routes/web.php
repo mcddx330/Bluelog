844a26ff06ae53f6ccceaa5f36a340dfd8abfe84
mcddx330 <monobeat.030+github@gmail.com>
2025-07-04 00:09:47 +0900

    route修正

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,14 @@ Route::get('/', function () {
 
 Route::get('/login', [BlueskyController::class, 'login'])->name('login');
 Route::post('/login', [BlueskyController::class, 'doLogin'])->name('login.post');
+Route::post('/logout', [BlueskyController::class, 'doLogout'])->name('logout');
 //Route::get('/posts/{accessJwt}/{refreshJwt}/{did}', [BlueskyController::class, 'showPosts'])->name('posts.show');
 //Route::get('/likes/{accessJwt}/{refreshJwt}/{did}', [BlueskyLikesController::class, 'showLikes'])->name('likes.show');
-Route::get('/profile/{handle}/likes', [BlueskyLikesController::class, 'show'])->name('profile.likes');
-Route::get('/profile/{handle}', [BlueskyController::class, 'showProfile'])->name('profile.show');
-Route::get('/profile/{handle}/status', [StatusController::class, 'show'])->name('profile.status');
-Route::get('/profile/{handle}/friends', [BlueskyController::class, 'showFriends'])->name('profile.friends');
-Route::get('/profile/{handle}/hashtags', [App\Http\Controllers\HashtagController::class, 'index'])->name('profile.hashtags');
+Route::get('/{handle}/likes', [BlueskyLikesController::class, 'show'])->name('profile.likes');
+Route::get('/{handle}', [BlueskyController::class, 'showProfile'])->name('profile.show');
+Route::get('/{handle}/status', [StatusController::class, 'show'])->name('profile.status');
+Route::get('/{handle}/friends', [BlueskyController::class, 'showFriends'])->name('profile.friends');
+Route::get('/{handle}/hashtags', [App\Http\Controllers\HashtagController::class, 'index'])->name('profile.hashtags');
 
 Route::middleware('auth')->group(function () {
     Route::get('/settings', [SettingsController::class, 'edit'])->name('settings.edit');
@@ -26,5 +27,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/settings', [SettingsController::class, 'destroy'])->name('settings.destroy');
     Route::post('/settings/export-posts', [SettingsController::class, 'exportPosts'])->name('settings.exportPosts');
 
-Route::post('/logout', [BlueskyController::class, 'doLogout'])->name('logout');
 });
